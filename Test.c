74df60743575690450a228ac5e17458654d98af9
Evan24sh <evan.o@outlook.fr>
2021-10-04 16:09:53 +0200

    v2

--- Test.c
+++ Test.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-int normaliseCapteur( int valCapteur[4] );
+//Déclaration des varibales globales
+int valCapteur[8] = {2,56,180,60,10,1020,65,55};
+unsigned long duree;
+struct timeval debutCalcul, finCalcul;
+
+
+//Calcule le temps et affiche la duree et la moyenne
+void info (float moyenne)
+{
+    tempsjour(&finCalcul,NULL);
+    printf("MOYENNE = %.2f",moyenne);
+    duree = ((finCalcul.tv_sec - debutCalcul.tv_sec) * 1000000) + (finCalcul.tv_usec - debutCalcul.tv_usec);
+    printf( "Duree %ld ms\n", duree );  
+}
+
+//tri le tableau dans l'ordre croisant
+void sortie_valCapteur ()
+{
+int tampon = 0;
+for (int i =1 ; i <= 7; i++); 
+	{
+    int j = i;
+    while (j > 0 && valCapteur[j-1] > valCapteur[j]) 
+		{
+      tampon = valCapteur[j];
+      valCapteur[j] = valCapteur[j-1];
+      valCapteur[j-1] = tampon;
+      j--;
+    	}
+  	}
+}
+
+//Calcule la moyenne
+float moyenne ()
+{
+int somme = 0;
+float moyenne =0.00;
+for (int b = 2; b!=6 ;b++)
+	{
+	somme = somme + valCapteur[b];
+	}
+moyenne = somme / 4;
+return moyenne ;
+}
 
 int main(void)
 {
-    int valCapteur[8] = { 56, 60, 65, 55 };
-    int moyenne;
+tempsjour(&debutCalcul,NULL);
 
-    int j;
-    unsigned long duree;
-    struct timeval debutCalcul, finCalcul;
+sortie_valCapteur();
 
-    // Heure debut du calcul
-    gettimeofday(&debutCalcul,NULL);
-
-    // Refaire le calcul 10000 fois pour augmenter la durée de calcul
-    for( j = 0; j < 1; j++) {
-        moyenne = normaliseCapteur(valCapteur);
-    }
-
-    // Heure fin du calcul
-    gettimeofday(&finCalcul,NULL);
-
-    printf("moyenne : %d\n", moyenne);
-
+print_all_info(moyenne());
 }
-
-int normaliseCapteur( int valCapteur[4] )
-{
-    int i;
-    int total=0;
-    int moyenne;
-    int valeur = 0; 
-    
-
-    total = 0;
-    for( i = 0; i < 4; i++) {
-        total += valCapteur[i];
-    }
-    
-   /* moyenne = total / 4; */
-    moyenne = total >> 2; 
-    printf("test nouvelle variable  \n"); 
-  
-    return(moyenne);
-}
-
